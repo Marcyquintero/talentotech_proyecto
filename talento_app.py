c52ef852e8ce798430a8e0a9f45abdf9cd741071
Marcy Quintero Castaño <110513032+Marcyquintero@users.noreply.github.com>
2025-02-17 07:12:06 -0500

    Update talento_app.py

---
+++ talento_app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy as sc
+import seaborn as sns
+import plotly.express as px
+from datetime import datetime
+
+st.set_page_config(
+  page_title="Dashboard Interactivo",
+  page_icon="",
+  layout="wide"
+)
+st.title("Dashboard interactivo con Streamlit")
+st.sidebar.title(""Opciones de navegacion")
+  
