f867425b465a30481e874fc23d136cea79def912
Marcy Quintero Castaño <110513032+Marcyquintero@users.noreply.github.com>
2025-02-25 14:25:23 -0500

    Update entrenamiento.py

--- entrenamiento.py
+++ entrenamiento.py
@@ -247,4 +247,4 @@ elif menu == "Percentiles":
     st_folium(mapa, width=700, height=400)
 
 if __name__ == "__main__":
-    st.sidebar.info("Ejecuta este script con: streamlit run solaris_app.py")
+    st.sidebar.info("Ejecuta este script con: streamlit run entrenamiento_app.py")
